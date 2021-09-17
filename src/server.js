5dd8af1ddbf360d9b9073835b597acb2abe61f7b
deathnet <53890887+deathnet@users.noreply.github.com>
2021-09-17 12:02:40 +0900

    chapter1 Chat with WebSocket 완료
    개선점
    1. 백에서 보내는 메시지 종류가 여러개 일 수 있다는 것을 프론트가 고려하지 못함
    2. 메시지 보낸 사람과 받는 사람 모두 같은 형식으로 메시지가 출력됨

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+// http WebSocket 함께 작동, 둘 다 같은 포트에서 동작
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✔");
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  socket.on("message", (msg) => {
+    const data = Buffer.from(msg, "base64").toString("utf8");
+    const message = JSON.parse(data);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+      default:
+    }
+  });
+  socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
